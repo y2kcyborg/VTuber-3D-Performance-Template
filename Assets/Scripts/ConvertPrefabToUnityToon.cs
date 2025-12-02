a7adf900609b11d786ced77cfa4778eff01fe46f
Millennium Cyborg <y2k.cyborg@gmail.com>
2025-12-02 22:38:14 +0000

    Material and prefab converter

--- Assets/Scripts/ConvertPrefabToUnityToon.cs
+++ Assets/Scripts/ConvertPrefabToUnityToon.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
 using UnityEditor.UIElements;
@@ -9,6 +10,12 @@ public class ConvertPrefabToUnityToon : EditorWindow
     [SerializeField]
     private VisualTreeAsset m_VisualTreeAsset = default;
 
+    private Dictionary<string, Material> convertedMats;
+
+    private static Shader s_unityToonShader;
+
+    private static int MainTex = Shader.PropertyToID("_MainTex");
+    
     [MenuItem("Window/VRM/ConvertPrefabToUnityToon")]
     public static void ShowExample()
     {
@@ -38,6 +45,8 @@ public class ConvertPrefabToUnityToon : EditorWindow
             return;
         }
 
+        s_unityToonShader = Shader.Find("Toon");
+
         if (PrefabUtility.GetPrefabAssetType(gameObject) == PrefabAssetType.NotAPrefab)
         {
             Debug.LogError($"{obj.name} is not a prefab!");
@@ -48,8 +57,65 @@ public class ConvertPrefabToUnityToon : EditorWindow
         Debug.Log($"Prefab path: '{prefabPath}'");
 
         string newPrefabPath =
-            $"{Path.GetDirectoryName(prefabPath)}/{Path.GetFileNameWithoutExtension(prefabPath)}_UnityToon.asset";
+            $"{Path.GetDirectoryName(prefabPath)}\\{Path.GetFileNameWithoutExtension(prefabPath)}_UnityToon.asset";
         Debug.Log($"Converted prefab path: '{newPrefabPath}'");
 
+        GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath);
+
+        var prefabInstance = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+
+        ConvertMaterials(prefabInstance);
+
+        PrefabUtility.SaveAsPrefabAssetAndConnect(prefabInstance, newPrefabPath, InteractionMode.UserAction);
+    }
+
+    public void ConvertMaterials(GameObject prefabInstance)
+    {
+        convertedMats = new();
+        var renderers = prefabInstance.GetComponentsInChildren<Renderer>();
+        foreach (var renderer in renderers)
+        {
+            ConvertMaterials(renderer.sharedMaterials);
+        }
+        var renderersSkinned = prefabInstance.GetComponentsInChildren<SkinnedMeshRenderer>();
+        foreach (var renderer in renderersSkinned)
+        {
+            ConvertMaterials(renderer.sharedMaterials);
+        }
+    }
+
+    public void ConvertMaterials(Material[] sharedMaterials)
+    {
+        for (int i = 0; i < sharedMaterials.Length; ++i)
+        {
+            var oldMat = sharedMaterials[i];
+            string oldPath = AssetDatabase.GetAssetPath(oldMat);
+
+            if (convertedMats.TryGetValue(oldPath, out Material newValue))
+            {
+                sharedMaterials[i] = newValue;
+                continue;
+            }
+
+            string newPath = $"{Path.GetDirectoryName(oldPath)}\\{Path.GetFileNameWithoutExtension(oldPath)}_UnityToon.mat";
+
+            var newMat = ConvertMaterial(oldMat);
+
+            AssetDatabase.CreateAsset(newMat, newPath);
+            
+            convertedMats[oldPath] = newMat;
+        }
+    }
+    
+    public Material ConvertMaterial(Material mat)
+    {
+        var newMat = new Material(s_unityToonShader);
+        
+        // Let's start with the simplest thing
+        newMat.SetTexture(MainTex, mat.GetTexture(MainTex));
+        newMat.SetTextureOffset(MainTex, mat.GetTextureOffset(MainTex));
+        newMat.SetTextureScale(MainTex, mat.GetTextureScale(MainTex));
+
+        return newMat;
     }
 }
